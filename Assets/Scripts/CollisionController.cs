19ea397bb36fe02422fbb43e890fac7cb6492a60
Mythricaluk <jparrish2026@stu.gvisd.org>
2025-02-28 09:10:29 -0600

    fridays work not done

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -1,6 +1,8 @@
+using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI
 
 public class CollisionController : MonoBehaviour
 {
@@ -9,66 +11,88 @@ public class CollisionController : MonoBehaviour
 
     public bool destroyEnemy;
     public bool destroyCollectibles;
-    public float pushPower = 2.0f;
 
     public AudioClip collisionAudio;
     private AudioSource audioSource;
-
+    
+    public float pushPower;
+    
+    public TMPro.TMP_Text scoreUI;
+    public int increaseScore = 1;
+    public int decreaseScore = 1;
+    private int score = 0;
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+       audioSource = GetComponent<AudioSource>(); 
     }
-
-    // only for GameObjects with a mesh, box, or other collider except for character controller and wheel colliders
+    void Update()
+    {
+        if (scoreUI != null)
+        {
+            scoreUI.text = score.ToString();
+        }
+    }
     void OnCollisionEnter(Collision other)
     {
-        if(changeColor == true){
+        if (changeColor == true)
+        {
             gameObject.GetComponent<Renderer>().material.color = myColor;
         }
-
-        if(audioSource != null && !audioSource.isPlaying){
-            audioSource.PlayOneShot(collisionAudio, 0.5F);
-        }
-
-        if(destroyEnemy == true && other.gameObject.tag == "Enemy" || destroyCollectibles == true && other.gameObject.tag == "Collectible"){
-            Destroy(other.gameObject);
-        }
-    }
-
-    // only for GameObjects with a character controller applied
-    void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Rigidbody body = hit.collider.attachedRigidbody;
-
-        // If no Rigidbody or is Kinematic, do nothing
-        if (body == null || body.isKinematic)
-        {
-            return;
-        }
-
-        // Don't push ground or platform GameObjects below character
-        if (hit.moveDirection.y < -0.3)
-        {
-            return;
-        }
-
-        // Calculate push direction from move direction, only along x and z axes - no vertical or y-axis pushing
-        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-        // Apply the push power and pushing direction to the pushed object's velocity
-        if (hit.gameObject.tag == "Object")
-        {
-            body.velocity = pushDir * pushPower;
-        }
-
+        
         if (audioSource != null && !audioSource.isPlaying)
         {
             audioSource.PlayOneShot(collisionAudio, 0.5F);
         }
+        
+        if (destroyEnemy == true && other.gameObject.tag == "Enemy" || destroyCollectibles == true && other.gameObject.tag == "Collectible")
+        {
+            Destroy(other.gameObject);
+        }
+        if (scoreUI != null && other.gameObject.tag == "Collectible")
+        {
+            score += increaseScore;
+        }
+        if (scoreUI != null && other.gameObject.tag == "Enemy")
+        {
+            score -= decreaseScore;
+        }
+    }
+    
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+        
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
 
-        if (destroyEnemy == true && hit.gameObject.tag == "Enemy" || destroyCollectibles == true && hit.gameObject.tag == "Collectible")
+        if (hit.gameObject.tag == "Object")
+        {
+            body.velocity = pushDir * pushPower;
+        }
+        
+        if(audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(collisionAudio, 0.5F);
+        }
+
+        if(destroyEnemy == true && hit.gameObject.tag == "Enemy" || destroyCollectibles == true && hit.gameObject.tag == "Collectible")
         {
             Destroy(hit.gameObject);
+        }
+        if (scoreUI != null && hit.gameObject.tag == "Collectible")
+        {
+            score += increaseScore;
+        }
+        if (scoreUI != null && hit.gameObject.tag == "Enemy")
+        {
+            score -= decreaseScore;
         }
     }
 }
